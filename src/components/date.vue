3a4845a294735c9c53063d668d27ac7c733f4fe8
Joseph <167524188+Joseph051006@users.noreply.github.com>
2026-02-26 13:06:51 +0100

    Date component
    I created an datecomponent, for the pubishdate of the jobdescription

---
+++ src/components/date.vue
@@ -0,0 +1,30 @@
+<script>
+
+    export default{
+        props: {
+            date: {
+                type: date,
+                required: true
+            },
+            time: {
+                type: time,
+                required: true
+            }
+        }
+    }
+
+</script>
+
+<template>
+
+    <p>Ausgestellt am: {{ date }} - {{ time }}</p>
+
+</template>
+
+<style scoped>
+
+    p {
+        color: rgb(210, 209, 209)
+    }
+
+</style>
