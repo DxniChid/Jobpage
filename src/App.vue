24017720b046fefa1e3d662d5587241bf958ec1a
Joseph <167524188+Joseph051006@users.noreply.github.com>
2026-03-23 09:34:30 +0100

    Fixed Footer
    The alignment of the footer was very off

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,46 @@
-
 <script setup>
 import { useRoute, useRouter } from 'vue-router'
 const route = useRoute()
-const router = useRouter() 
+const router = useRouter()
 import './assets/style.css'
 function logout() {
-  router.push({ name: 'login' }); 
+  router.push({ name: 'login' });
 }
 </script>
 
 <template>
-<header v-if="route.name !== 'login' && route.name !== 'register'">
-  <Header title="Job Page" icon="src/images/pfp.webp"></Header>
+  <div id="app">
+  <header v-if="route.name !== 'login' && route.name !== 'register'">
+    <Header title="Job Page" icon="src/images/pfp.webp"></Header>
     <h3 @click="$router.push('/favorites')">Meine Favoriten</h3>
-    <img src="/src/images/logo-header.png" alt="Papagei-Logo" @click="$router.push('/homepage')" class="logo-header">    
+    <img src="/src/images/logo-header.png" alt="Papagei-Logo" @click="$router.push('/homepage')" class="logo-header">
     <h1>ParrotJobs</h1>
     <div class="profile">
       <span>A.B.</span>
       <button @click="logout">Ausloggen</button>
     </div>
   </header>
+  <main> 
   <RouterView></RouterView>
-<footer v-if="route.name !== 'login' && route.name !== 'register'">
-  <h4>079 90 00 00</h4>
-  <h4>test@gmail.com</h4>
-  
-</footer>
-
+  </main>
+    <footer v-if="route.name !== 'login' && route.name !== 'register'">
+      <h4>079 90 00 00</h4>
+      <h4>test@gmail.com</h4>
+    </footer>
+ </div>
 </template>
+
 <style scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+main {
+  flex: 1;
+}
+
 h1 {
   margin-bottom: 20px;
 }
